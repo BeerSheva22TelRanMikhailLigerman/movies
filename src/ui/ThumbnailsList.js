1dc9981389c121c214be32e23b116bf7802a56b2
LigermanM <Ligerman.m@gmail.com>
2023-06-08 14:44:46 +0300

    popList, no details

--- src/ui/ThumbnailsList.js
+++ src/ui/ThumbnailsList.js
@@ -1,42 +1,49 @@
 const ACTIVE = 'active'
 export default class ThumbnailsList {
-    #buttons
+    #thumbnail
     #sectionElements
     #activeIndex
     #callbackFn
-    constructor(parentId, thumbnailsData, callbackFn) {        
+    constructor(parentId, thumbnailsData, callbackFn) {
         this.#callbackFn = callbackFn;
-        this.#fillThumbnails(parentId, thumbnailsData.map(s => s.title));
+        this.#fillThumbnails(parentId, thumbnailsData);
         console.log(thumbnailsData)
         this.#setSectionElements(thumbnailsData.map(s => s.id));
         this.#addListeners();
 
 
     }
-    #fillThumbnails(parentId, titles) {
+    #fillThumbnails(parentId, thumbnailData) {
         const parentElement = document.getElementById(parentId);
-        parentElement.innerHTML = titles.map(t => `<div class="menu-button">${t}</div>`).join('');
-        this.#buttons = parentElement.childNodes;
+        parentElement.innerHTML = thumbnailData.map(t => `
+            <li class="thumbnails-item">
+                <a href="#" class="thumnails-ancor">
+                <img src="${t.backdrop_path}" class="thumnails-image">
+                <span class="thumbnails-title">${t.original_title}</span>
+                </a>
+            </li>
+        `);
+        this.#thumbnail = parentElement.childNodes;
     }
     #setSectionElements(sectionIds) {
         this.#sectionElements = sectionIds.map(id => document.getElementById(id));
     }
     #addListeners() {
-        this.#buttons.forEach((b, index) => b.addEventListener('click', this.#handler.bind(this, index)))
+        this.#thumbnail.forEach((b, index) => b.addEventListener('click', this.#handler.bind(this, index)))
     }
     async #handler(index) {
         if (this.#activeIndex == undefined || index != this.#activeIndex) {
-            
-            if(this.#activeIndex != undefined) {
-                 this.#buttons[this.#activeIndex].classList.remove(ACTIVE);
-                 this.#sectionElements[this.#activeIndex].hidden = true;
+
+            if (this.#activeIndex != undefined) {
+                this.#thumbnail[this.#activeIndex].classList.remove(ACTIVE);
+                this.#sectionElements[this.#activeIndex].hidden = true;
             }
-             
-             await this.#callbackFn(index);
-             this.#sectionElements[index].hidden = false;
-             this.#buttons[index].classList.add(ACTIVE);
-             this.#activeIndex = index;
-            
+
+            await this.#callbackFn(index);
+            this.#sectionElements[index].hidden = false;
+            this.#thumbnail[index].classList.add(ACTIVE);
+            this.#activeIndex = index;
+
 
         }
     }
